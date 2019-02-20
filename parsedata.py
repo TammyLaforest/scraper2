995e7876d9ec44460cd7bc0b8f6a6243dceb2ff6
TammyLaforest <TammyLaforest@gmail.com>
2019-02-20 18:13:51 -0500

    scraper

---
+++ parsedata.py
@@ -0,0 +1,13 @@
+import json
+with open('twitterData.json') as json_data:
+    jsonData = json.load(json_data)
+
+for i in jsonData:
+	print(i['date'], i['likes'])
+
+# for i in jsonData:
+# 	print(i['date'])
+
+# for i in jsonData:
+#     if "obama" in i['tweet'].lower():
+#         print(i)
